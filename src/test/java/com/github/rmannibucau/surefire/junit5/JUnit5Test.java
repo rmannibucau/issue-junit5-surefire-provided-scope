9bbf8c296d5498dd63e5c20f7ef1b96d88765a15
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-09-18 09:12:24 +0200

    first commit

---
+++ src/test/java/com/github/rmannibucau/surefire/junit5/JUnit5Test.java
@@ -0,0 +1,13 @@
+package com.github.rmannibucau.surefire.junit5;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class JUnit5Test {
+    @Test
+    void plainOldJUnitTest() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/com/github/rmannibucau/surefire/junit5/SomeTest.java
@@ -0,0 +1,12 @@
+package com.github.rmannibucau.surefire.junit5;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SomeTest {
+    @Test
+    public void plainOldJUnitTest() {
+        assertTrue(true);
+    }
+}
